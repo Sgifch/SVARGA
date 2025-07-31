13a07e62008eda74ff83dd18a767d60878088a39
yAnD9 <145001165+yAnD9@users.noreply.github.com>
2025-07-31 19:28:05 +0400

    Сделала меню настроек
    Пока только оболочка

--- Assets/Script/Paused.cs
+++ Assets/Script/Paused.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
+using UnityEngine.Audio; 
 
 public class Paused : MonoBehaviour
 {
     [SerializeField]
     GameObject pause;
+    [SerializeField]
+    GameObject settings;
+
+    public float volume = 0; //Громкость
+    public int quality = 0; //Качество
+    public bool isFullscreen = false; //Полноэкранный режим
+    public AudioMixer audioMixer; //Регулятор громкости
+    public Dropdown resolutionDropdown; //Список с разрешениями для игры
+    private Resolution[] resolutions; //Список доступных разрешений
+    private int currResolutionIndex = 0; //Текущее разрешение
 
     void Start()
     {
@@ -27,6 +39,36 @@ public class Paused : MonoBehaviour
     {
         pause.SetActive(false);
         Time.timeScale = 1;
+        settings.SetActive(false);
     }
 
+    public void Settings()
+    {
+        settings.SetActive(true);
+    }
+
+    public void ChangeVolume(float val) //Изменение звука
+    {
+        volume = val;
+    }
+
+    public void ChangeResolution(int index) //Изменение разрешения
+    {
+        currResolutionIndex = index;
+    }
+
+    public void ChangeFullscreenMode(bool val) //Включение или отключение полноэкранного режима
+    {
+        isFullscreen = val;
+    }
+
+    public void ChangeQuality(int index) //Изменение качества
+    {
+        quality = index;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
