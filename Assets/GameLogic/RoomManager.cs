016fd0ebdcbae4abe3ba744c8f3df12f32f6d31a
Sgifch <144769642+Sgifch@users.noreply.github.com>
2025-08-26 16:45:43 +0400

    Добавила спавн мобов в комнате
    Надо потом настроить все комнаты

--- Assets/GameLogic/RoomManager.cs
+++ Assets/GameLogic/RoomManager.cs
@@ -7,7 +7,7 @@ public class RoomManager : MonoBehaviour
 {
     public List<GameObject> wall = new List<GameObject>();
     public List<GameObject> triggerZone = new List<GameObject>();
-    public List<GameObject> enemy = new List<GameObject>(); //Лист мобов
+    public EnemyList enemy;
     //public List<GameObject> currentEnemy = new List<GameObject>();
     public GameObject currentEnemy;
     public GameObject spawnEffect;
@@ -22,7 +22,7 @@ public class RoomManager : MonoBehaviour
     void Start()
     {
         currentAmountEnemy = Random.Range(minSpawn, maxSpawn);
-        for (int i = 0; i<maxSpawn; i++)
+        for (int i = 0; i<currentAmountEnemy; i++)
         {
             EnemySpawn();
         }
@@ -43,9 +43,9 @@ public class RoomManager : MonoBehaviour
         int shiftPositionX;
         int shiftPositionY;
 
-        shiftPositionX = Random.Range(1, maxShiftPosition);
-        shiftPositionY = Random.Range(1, maxShiftPosition);
+        shiftPositionX = Random.Range(-maxShiftPosition, maxShiftPosition);
+        shiftPositionY = Random.Range(-maxShiftPosition, maxShiftPosition);
 
-        Instantiate(enemy[0], transform.position + new Vector3(shiftPositionX, shiftPositionY, 0), transform.rotation, currentEnemy.transform);
+        Instantiate(enemy.enemyList[enemy.enemyList.Count - 1], transform.position + new Vector3(shiftPositionX, shiftPositionY, 0), transform.rotation, currentEnemy.transform);
     }
 }
